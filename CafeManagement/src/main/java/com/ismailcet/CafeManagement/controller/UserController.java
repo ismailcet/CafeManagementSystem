8cafe85ceea0ed6fb21657f60f4f4de6bb2dd470
ismailcet <ismailcet3@gmail.com>
2023-04-11 01:40:19 +0300

    Created Repository , Service and Controller for User Entity.

---
+++ CafeManagement/src/main/java/com/ismailcet/CafeManagement/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.ismailcet.CafeManagement.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+}
---
+++ CafeManagement/src/main/java/com/ismailcet/CafeManagement/service/UserService.java
@@ -0,0 +1,7 @@
+package com.ismailcet.CafeManagement.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
