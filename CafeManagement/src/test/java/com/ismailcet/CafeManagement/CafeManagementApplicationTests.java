3eedf0c93d2ee3c905cc0d644c2ed174526994ff
ismailcet <ismailcet3@gmail.com>
2023-04-11 01:01:47 +0300

    Cafe Managment System Backend Files

---
+++ CafeManagement/src/test/java/com/ismailcet/CafeManagement/CafeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ismailcet.CafeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
