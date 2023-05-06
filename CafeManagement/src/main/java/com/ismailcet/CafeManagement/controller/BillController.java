0661b0a593eaad1f546b1d604a29e56ad3ab397b
ismailcet <ismailcet3@gmail.com>
2023-05-06 20:54:20 +0300

    All CRUD elements are complated for Bill Entity.And Backend Size is complated for Cafe Management System.

--- CafeManagement/src/main/java/com/ismailcet/CafeManagement/controller/BillController.java
+++ CafeManagement/src/main/java/com/ismailcet/CafeManagement/controller/BillController.java
@@ -1,15 +1,15 @@
 package com.ismailcet.CafeManagement.controller;
 
 import com.ismailcet.CafeManagement.constents.CafeConstants;
+import com.ismailcet.CafeManagement.entity.Bill;
 import com.ismailcet.CafeManagement.service.BillService;
 import com.ismailcet.CafeManagement.utils.CafeUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -31,5 +31,25 @@ public class BillController {
             ex.printStackTrace();
         }
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @GetMapping("/getBills")
+    public ResponseEntity<List<Bill>> getBills(){
+        try{
+            return billService.getBills();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @PostMapping("/getPdf")
+    public ResponseEntity<byte[]> getPdf(@RequestBody Map<String, Object> requestMap){
+        try{
+            return billService.getPdf(requestMap);
+        }catch (Exception ex){
+
+        }
+        return null;
     }
 }
--- CafeManagement/src/main/java/com/ismailcet/CafeManagement/repository/BillRepository.java
+++ CafeManagement/src/main/java/com/ismailcet/CafeManagement/repository/BillRepository.java
@@ -2,8 +2,17 @@ package com.ismailcet.CafeManagement.repository;
 
 import com.ismailcet.CafeManagement.entity.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BillRepository extends JpaRepository<Bill, Integer> {
+
+    @Query("SELECT b FROM Bill b ORDER BY b.id DESC")
+    List<Bill> getAllBills();
+
+    @Query("SELECT b FROM Bill b WHERE b.createdBy = ?1 ORDER BY b.id desc")
+    List<Bill> getBillByUserName(String username);
 }
